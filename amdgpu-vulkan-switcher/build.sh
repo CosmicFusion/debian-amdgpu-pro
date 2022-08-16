4872b59d59f75166026574b6142c2ce72ccfd9a7
Cosmic Fusion <83735213+CosmicFusion@users.noreply.github.com>
2022-08-16 12:59:26 +0300

    Update build.sh

--- amdgpu-vulkan-switcher/build.sh
+++ amdgpu-vulkan-switcher/build.sh
@@ -6,6 +6,14 @@ echo "making debian package"
 
 cd ./DEBIAN
 
+#
+
+echo "fixing permissions"
+
+chmod -R +rwx ./usr/bin/*
+
+#
+
 mkdir -p ./DEBIAN
 
 touch ./DEBIAN/control
@@ -18,7 +26,7 @@ cd ../
 
 #
 
-dpkg-deb --build ./DEBIAN
+dpkg-deb --build dpkg-deb --build --root-owner-group ./DEBIAN
 
 #
 
